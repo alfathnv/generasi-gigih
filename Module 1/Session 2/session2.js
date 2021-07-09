ed3a7809cbb31a1c89d8a4bac9705d434ff0e761
alfathnv <87052656+alfathnv@users.noreply.github.com>
2021-07-09 16:46:13 +0700

    update session2

--- Module 1/Session 2/session2.js
+++ Module 1/Session 2/session2.js
@@ -4,21 +4,39 @@ function myFunction() {
     alert("Ini Alert");
 }
 
-function get_api(url) {
-    
+// function get_api(url) {  
+//     fetch(url)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             show(data);
+//             console.log(data);
+//         })
+//         .catch((err) => {
+//             alert("Ini Alert Failed", err);
+//         });
+// }
+
+function get_api(url) {  
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        show(data)
-        console.log(data)})
-      .catch((err) => {
-        alert("Ini Alert Failed", err);
-    });
-}
+        .then((response) => {
+            if (!response.ok) {
+                Promise.reject("There is an error fetching the data"); //Keluar error kalau ada typo di request url
+            }
+        console.log("status ok, response");
+        return response.json();
+        })
+        .then((data) => {
+            console.log("resolved", data);
+            show(data);
+        })
+        .catch((err) => {
+            console.log("rejected:", err);
+        });
+};
 
 function show(data) {
     let playlist =
-        `<img src= ${data.album.images[0].url} alt="22miles" style="width: 100%;"><br>
+        `<img src= ${data.album.images[0].url} alt="image"><br>
         <p>Title</p>
         <p>${data.name}</p><br>
         <p>Artists</p>
